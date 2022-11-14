da50a70ac864049c6419cd901463791108f276fc
Sandeep Zechariah George <sandeepzgk@gmail.com>
2022-11-14 20:07:00 +0000

    removed git cache and commit again

--- build/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const block_1 = require("./block");
-const block = new block_1.Block();
-block.updateBlock(block_1.BlockType.regular_sine, 1, 440, 0, -1, 0, [1, 0, 0, 0], 0, '');
-block.writeWav('testfile.wav');
-//# sourceMappingURL=index.js.map
